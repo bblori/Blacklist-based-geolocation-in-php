b3734c753d9c1054331250530148c6b1631a4e49
lori <lori@loris-iMac.local>
2020-08-15 19:30:42 +0200

    ok

--- index.php
+++ index.php
@@ -11,16 +11,11 @@
 	getenv('REMOTE_ADDR');
 	
 	// Orszag meghatarozasa ip cim alapjan.
-    $json = file_get_contents("https://geolocation-db.com/json/".$ip);
-    
+    $json = file_get_contents("https://geolocation-db.com/json/".$ip);    
     $data = json_decode($json);
-    
     $latogato = $data->country_name.'<br>';
-
 	$latogatoOrszag = substr($latogato, 0, 2);
-		
-	$hossz = count($engedelyezettOrszag);	
-	
+
 	if (!in_array($latogatoOrszag, $engedelyezettOrszag)){
 		felvetel($ip);	
 		header('Location: http://fbi.gov/');
